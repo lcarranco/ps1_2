6a542159860f341802a0f8f8da7f62f8903e4e50
lcarranco <lcarranco@gmail.com>
2017-05-09 18:21:18 -0500

    Completed problem
    Ignoring .vscode folder

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    string str;
-    cin >> str;
+    // // Simple example
+    // int i = 4;
+    // double d = 4.0;
+    // string s = "HackerRank";
+    // cout << "\ti: " << i << endl;
+    // cout << setprecision(1) << fixed << "\td: " << d  << endl;
+    // cout << "\ts: " << s << endl;
+
+    string line;
+    getline(cin, line);
+    int i = stoi(line);
+
+    getline(cin, line);
+    double d = stod(line);
+
+    getline(cin, line);
+    string str = line;
+
+    string s = "HackerRank";
+    cout << i + d << endl;
+    cout << setprecision(1) << fixed << d + d  << endl;
+    cout << s + " " + str << endl;
+
     return 0;
 }
